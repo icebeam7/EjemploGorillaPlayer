5830483174e9ec9dd7921893b40c0832e9cb03e8
beltran_prieto <beltran_prieto@fai.utb.cz>
2016-10-07 18:30:00 +0200

    Gorilla Player con Xamarin.Forms

---
+++ EjemploGorillaPlayer/EjemploGorillaPlayer/PaginaFormulario.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EjemploGorillaPlayer
+{
+	public partial class PaginaFormulario : ContentPage
+	{
+		public PaginaFormulario ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
